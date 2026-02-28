b7c50c2542026b345442c7ec550b241aabaaaaa8
Zeynab Thiandoum <thiandoumzeynab8@gmail.com>
2026-02-28 12:32:31 +0000

    Fix: corriger la méthode saluer

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -5,7 +5,7 @@ public class App {
     if (nom == null || nom.isEmpty()) {
             throw new IllegalArgumentException("Le nom ne peut pas être vide");
         }
-        return "Bonjour, " + nom + " ,Bienvenue! ";
+        return "Bonjour, " + nom + " ! ";
     }
     public static void main(String[] args) {
         System.out.println(saluer("Jenkins"));
