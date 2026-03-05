f45d5597d478e7091f0b1bce5a81b4a628ba4164
Zeynab Thiandoum <thiandoumzeynab8@gmail.com>
2026-03-05 18:46:48 +0000

    Test error

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -5,7 +5,7 @@ public class App {
     if (nom == null || nom.isEmpty()) {
             throw new IllegalArgumentException("Le nom ne peut pas être vide");
         }
-        return "Bonjour, " + nom + " !";
+        return "Bonjour, " + nom + " !  ";
     }
     public static void main(String[] args) {
         System.out.println(saluer("Jenkins"));
